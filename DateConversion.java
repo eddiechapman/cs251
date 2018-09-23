29737fad8a4595c20860e014caf7a2c53e285084
eddiechapman <33849157+eddiechapman@users.noreply.github.com>
2018-09-22 19:11:27 -0500

    exception handling

--- DateConversion.java
+++ DateConversion.java
@@ -12,76 +12,128 @@ public class DateConversion {
 	private static String[] fullNameMonths = { "january", "february", "march", "april", "may", "june", "july", "august", "september",
 			"october", "november", "december" };
 
+	private static String dashError = "ERROR: Too many dashes";
+	private static String monthError = "ERROR: Invalid month string";
+	private static String dayError = "ERROR: Invalid month or day number";
+	private static String inputError = "ERROR: Empty input line";
+	private static String yearError = "ERROR: Invalid Year -- too low or high";
+	private static String andError = "ERROR: No date entered";
+	
 	public static void main(String[] args) {
-
+		
 		Scanner stdIn = new Scanner(System.in);
-
 
 		System.out.println("Welcome to the CS251 Date Converter!\n");
 
 		System.out.print("Enter line of dates:");
 		String dateEntries = stdIn.nextLine();
-
-		// Split dateEntries into array of dateString
-		// For each dateString in array:
-		//		if it looks like dateFirst:
-		//			parseDayFirst(dateEntries)
-		//		if it looks like monthFirst:
-		//			parseMonthFirst(dateEntries)
-		//		if it looks like allNumbers:
-		//			parseAllNumbers(dateEntries)
 		
-		// the parsing methods will either print the date or an error
+		if (dateEntries.isEmpty()) {
+			System.out.print(inputError);
+		}
+		else {
+			String[] dateEntriesArray = dateEntries.split("and");
+			
+			for (int i = 0; i < dateEntriesArray.length; i++) {
+				String dateStr = dateEntriesArray[i].trim();
+				
+				if (dateStr.isEmpty()) {
+					System.out.print("\nDate " + (i + 1) + ": " + andError);
+				} else if (dateStr.indexOf('-') != -1) {
+					System.out.print("\nDate " + (i + 1) + ": ");
+					parseAllNumbers(dateStr);
+				} else if (dateStr.indexOf(',') != -1) {
+					System.out.print("\nDate " + (i + 1) + ": ");
+					parseMonthFirst(dateStr);
+				} else { 
+					System.out.print("\nDate " + (i + 1) + ": "); 
+					parseDayFirst(dateStr);
+				}
+			}  // end for loop
+		} // end else
 		
-		System.out.println("\nGoodbye!");
-
+		
+		
+		System.out.println("\n\nGoodbye!");
+		
 		stdIn.close();
-	}
+		
+	}  // end main
 
-	
+
 	public static void parseDayFirst(String dateStr) {
 
-	/*	break dateStr into compontent parts
-	 * 	check if isValidMonthAbbr
-	 * 	convert month to int with monthToNumber
-	 * 	check if isValidMonthDay
-	 * 	check if isValid Year
-	 * 	print standardDateString 
-	 */
-
+		int day = Integer.parseInt(dateStr.substring(0, dateStr.indexOf(" ")).trim());
+		String month = dateStr.substring(dateStr.indexOf(" "), (dateStr.length() - 4)).trim();
+		int year = Integer.parseInt(dateStr.substring((dateStr.length() - 4)).trim());
+		
+		if (isValidMonthAbbr(month) == false) {
+			System.out.print(monthError);
+		} else if (isValidMonthDay(day, monthToNumber(month)) == false) {
+			System.out.print(dayError);
+		} else if (isValidYear(year) == false) {
+			System.out.print(yearError);
+		} else {
+			System.out.print(standardDateString(day, monthToNumber(month), year));
+		}
 	}
-	
+
 
 	public static void parseMonthFirst(String dateStr) {
+		
+		String month = dateStr.substring(0, dateStr.indexOf(" ")).trim();
+		int year = Integer.parseInt(dateStr.substring(dateStr.indexOf(",") + 1).trim());
+		int day = Integer.parseInt(dateStr.substring(dateStr.indexOf(" "), dateStr.indexOf(",")).trim());
+		
+		if (isValidMonthAbbr(month) == false) {
+			System.out.print(monthError);
+		} else if (isValidMonthDay(day, monthToNumber(month)) == false) {
+			System.out.print(dayError);
+		} else if (isValidYear(year) == false) {
+			System.out.print(yearError);
+		} else {
+			System.out.print(standardDateString(day, monthToNumber(month), year));
+		}
 
-		/*	break dateStr into compontent parts
-		 * 	check if isValidMonthAbbr
-		 * 	convert month to int with monthToNumber
-		 * 	check if isValidMonthDay
-		 * 	check if isValid Year
-		 * 	print standardDateString 
-		 */
-
-	}
+	} // end parseMonthFirst
 	
 	public static void parseAllNumbers(String dateStr) {
-
-		/*	break dateStr into compontent parts
-		 * 	check if isValidMonthDay
-		 * 	check if isValid Year
-		 * 	print standardDateString 
-		 */
-
-	}
+		
+		if (dateStr.length() - dateStr.replace("-", "").length() > 2) {
+			System.out.print(dashError);
+		}
+		
+		int month = Integer.parseInt(dateStr.substring(0, dateStr.indexOf("-")).trim());
+		int day = Integer.parseInt(dateStr.substring((dateStr.indexOf("-") + 1),  (dateStr.length()-5)).replace("-", "").trim());
+		int year = Integer.parseInt(dateStr.substring((dateStr.length() - 5)).trim());
+		
+		if (isValidMonthDay(day, month) == false) {
+			System.out.print(dayError);
+		} else if (isValidYear(year) == false) {
+			System.out.print(yearError);
+		} else {
+			System.out.print(standardDateString(day, month, year));
+		}
+		
+	} // end parseAllNumber
 
 	
 	public static boolean isValidMonthDay(int day, int month) {
-
-		/*
-		 * Use switch to pick a month
-		 * The conditions within each month list the legal range of days
-		 */
-	}
+		
+		if (day < 1) { return false; }
+		
+		switch (month) {
+		  	case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+		  		return day <= 31 ? true : false;
+	 		case 4: case 6: case 9: case 11:
+	  			return day <= 30 ? true : false;
+	  		case 2:
+	 			return day <= 28 ? true : false;
+	  		default: 
+	  			return false;
+	  	} // end switch
+		
+	} // end isValidDay
 
 	
 	public static int monthToNumber(String monthStr) {
@@ -90,8 +142,8 @@ public class DateConversion {
 
 		int mNumber = 0;
 
-		for(int i = 0; i < abbrMonths.length; i++){
-			if(abbrMonths[i].equals(lowerCaseMonthStr)){
+		for (int i = 0; i < abbrMonths.length; i++) {
+			if (abbrMonths[i].equals(lowerCaseMonthStr)) {
 				mNumber = i + 1;
 				break;
 			}
@@ -99,24 +151,40 @@ public class DateConversion {
 
 		return mNumber;
 
-	}
+	} // end monthToNumber
 
 
 	public static boolean isValidMonthAbbr(String str) {
-
-		//TODO
 		
-	}
+		if (str.length() < 3) { return false; }
+		
+		for (String month: abbrMonths) {
+			if (month.equalsIgnoreCase(str.substring(0, 3)) == true) {
+				return true ; 
+			} else { 
+				continue; 
+			}
+		} // end for loop
+		
+		return false;
+		
+	} // end isValidMonthAbbr
 
 
 	public static boolean isValidYear(int year) {
-		//TODO
-	}
+	
+		return year >= 1900 && year <= 2018 ? true : false ;
+
+	} // end isValidYear
 
 	
 	public static String standardDateString(int d, int m, int y) {
 		
-		//TODO
-		
-	}
-}
+		String month = fullNameMonths[m-1].substring(0,1).toUpperCase() 
+				+ fullNameMonths[m-1].substring(1);
+	
+		return d + " " + month + " " + y;
+	 
+	} // end standardDateString
+	
+} // end DateConversion
