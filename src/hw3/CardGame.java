0bd4e716e7cecc57aec917f97f84a35f1dd914b5
eddiechapman <eddie.chapman4@gmail.com>
2018-09-27 10:25:00 -0500

    resetDeck method

--- src/hw3/CardGame.java
+++ src/hw3/CardGame.java
@@ -6,7 +6,41 @@ public class CardGame {
 	private static final String[] cardNumbers = {"2","3","4","5","6","7","8","9","10","J","Q","K","A"};
 	private static final String[] cardSuits = {"D","H","C","S"};
 
-	//TODO
+	private boolean[] deck;
+	private String[][] players;
+	private int cardsDealt;
+	private boolean hasDealtCards;
 	
+	public CardGame(int totalPlayers) {
+		return;
+	}
+	
+	public int getTotalCardsDealt() {
+		return;
+	}
+	
+	public void resetDeck() {
+		return;
+	}
+	
+	public void dealHands() {
+		return;
+	}
+	
+	private String dealCard() {
+		return;
+	}
+	
+	private String convertCard(int card) {
+		return;
+	}
+	
+	public void addPlayers(int playersToAdd) {
+		return;
+	}
+	
+	public void displayHands() {
+		return;
+	}
 	
 }
