1c082b5c90c3012228e1dee9314f81f50f458297
eddiechapman <eddie.chapman4@gmail.com>
2018-10-06 20:39:17 -0500

    Documentation

--- src/hw4/MovieCollection.java
+++ src/hw4/MovieCollection.java
@@ -2,7 +2,7 @@
  * MovieCollection.java
  * Eddie Chapman
  * 
- * TODO: Insert description here
+ * This class maintains a collection of movie objects.
  *******************************************************************************/
 
 package hw4;
@@ -14,22 +14,13 @@ public class MovieCollection
 
 	//***************************************************************************
 	
-	/**
-	 * Constructor.
-	 * You are only allowed to hold 10 movies. 
-	 * Initialize the array.
-	 */
 	public MovieCollection()
 	{
-		movies = new Movie[10];
+		movies = new Movie[10];		// Maximum 10 movies per collection
 	}
 
 	//***************************************************************************
 	
-	/**
-	 * Return the total number of movies.
-	 * @return int
-	 */
 	public int getTotalMovies()
 	{
 		return movieCount;
@@ -39,8 +30,6 @@ public class MovieCollection
 	
 	/**
 	 * Add the passed in Movie to your collection if there is space for it.
-	 * You are not allowed duplicate copies of a movie in the array.
-	 * Make sure to check movieCount to make sure it can be added.
 	 * If the movie is added return true.  Else return false.
 	 * 
 	 * @param movie
@@ -61,8 +50,6 @@ public class MovieCollection
 	
 	/**
 	 * Add a movie at the specifed index if the index is valid.
-	 * You must verify the index.  Remember, no duplicate movies are allowed.
-	 * You will need to shift all your movies to the right to make room for the new movie.
 	 * Return true if movie was added, false otherwise.
 	 * 
 	 * @param movie
@@ -91,9 +78,6 @@ public class MovieCollection
 	
 	/**
 	 * Shift all movies to the right based on the index passed in.
-	 * This will create 'space' for a new movie to be added.
-	 * Should only be called by addMovieAt().
-	 * Private methods of a class are considered helper methods.
 	 * 
 	 * @param index
 	 */
@@ -110,10 +94,7 @@ public class MovieCollection
 	
 	/**
 	 * Find location of the passed in Movie and return its location in the array.
-	 * This means a value from [0, movieCount) could be returned if it is in the array.
 	 * If the movie is not there, return -1 as a value.
-	 * To compare movies, you only need to compare the name and runtime.
-	 * Remember, you can use methods in Movie class to easily compare two movies now.
 	 * 
 	 * @param movie
 	 * @return int
@@ -135,7 +116,6 @@ public class MovieCollection
 	/**
 	 * Get the movie at the specified index and return it.
 	 * If the index passed in is invalid, return null.
-	 * No shifting required.
 	 * 
 	 * @param index
 	 * @return Movie
@@ -153,9 +133,6 @@ public class MovieCollection
 	
 	/**
 	 * Remove the passed in Movie if it is there.
-	 * If the movie is there, you will need to "shift" the array backwards one location to "remove" the movie.
-	 * Remember, no null spaces are allowed in the array.
-	 * You will need to call shiftCollectionLeft if you remove a movie.
 	 * This method returns true if the movie is removed, false otherwise.
 	 * 
 	 * @param movie
@@ -176,8 +153,6 @@ public class MovieCollection
 	
 	/**
 	 * Remove the Movie at the specified index.
-	 * Make sure the index is a valid index.
-	 * You will need to call shiftCollectionLeft to remove the movie.
 	 * Return the Movie that was at this location, or null if an invalid index was used.
 	 * 
 	 * @param index
@@ -200,13 +175,9 @@ public class MovieCollection
 	
 	/**
 	 * Move all elements after the index one location backwards in the array.
-	 * This method is meant to be called by removeMovie() and removeMovieAt() and is private.
-	 * Private methods of a class are considered helper methods.
-	 * The parameter index is meant to be the location of the element to be removed.
 	 * 
 	 * @param index
 	 */
-	
 	private void shiftCollectionLeft(int index)
 	{
 		for (int i=index; i<movieCount; i++)
@@ -249,12 +220,8 @@ public class MovieCollection
 
 	//***************************************************************************
 	
-	/**
-	 * Print out all movies that are considered rotten in the array.
-	 * Remember you can use methods in the Movie class to determine this.
-	 * Refer to driver and handout for output format.
-	 * 
-	 */
+	 // Print out all movies that are considered rotten in the array.
+
 	public void moviesToAvoid()
 	{
 		int badMovieCount = 0;
@@ -269,16 +236,12 @@ public class MovieCollection
 				System.out.println("Tomato Rating: Rotten");
 			}
 		}
-	}
+	} // end moviesToAvoid
 
 	//***************************************************************************
 	
-	/**
-	 * Print out all movies that are considered fresh in the array.
-	 * Remember you can use methods in the Movie class to determine this.
-	 * Refer to driver and handout for output format.
-	 * 
-	 */
+	// Print out all movies that are considered fresh in the array
+
 	public void moviesToWatch()
 	{
 		int goodMovieCount = 1;
@@ -293,22 +256,18 @@ public class MovieCollection
 				System.out.println("Tomato Rating: Fresh");
 			}
 		}
-	}
+	} // end moviesToWatch
 
 	//***************************************************************************
-	
-	/**
-	 * Print out all movies in the array.
-	 * You only need to print out the name of the movie, nothing more.
-	 * Refer to driver and handout for output format.
-	 * 
-	 */
+
+	// Print out all movies in the array.
+
 	public void printOutMovieList()
 	{
 		System.out.println("\nAll of my movies:");
 		for (int i=0; i<movieCount; i++)
 		{
-			System.out.println("Movie" + (i+1) + ": " + movies[i].getName());
+			System.out.println("Movie" + (i + 1) + ": " + movies[i].getName());
 		}
 	}
 
