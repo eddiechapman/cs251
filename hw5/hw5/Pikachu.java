3d4d4022e28b1a05e7231cda63c1da83b823f547
eddiechapman <eddie.chapman4@gmail.com>
2018-10-08 12:49:36 -0500

    Add variables and method headers

--- hw5/hw5/Pikachu.java
+++ hw5/hw5/Pikachu.java
@@ -1,7 +1,51 @@
+/********************************************************************************
+ * Pikachu.java
+ * Eddie Chapman
+ * 
+ * TODO: class description
+ *******************************************************************************/
+
 package hw5;
 
 public class Pikachu extends Pokemon {
 
-	//TODO
+	private final int thunderBolt = 6;
+	private final int swipe = 3;
 	
-}
+	//***************************************************************************
+	
+	public Pikachu(int health, int power, int level)
+	{
+		//TODO
+	} // end Pikachu
+	
+	//***************************************************************************
+	
+	@Override
+	public String toString()
+	{
+		//TODO
+	} // end toString
+	
+	//***************************************************************************
+	
+	@Override
+	public boolean equals(Object other)
+	{
+		//TODO
+	} // end equals
+	
+	//***************************************************************************
+	
+	public void specialAttack(Pokemon target)
+	{
+		//TODO
+	} // end specialAttack
+	
+	//***************************************************************************
+	
+	public void physicalAttack(Pokemon target)
+	{
+		//TODO
+	} // end physicalAttack
+} // end class Pikachu
