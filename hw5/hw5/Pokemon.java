6a6fdf43b7aae2d51da609f56c759bab4b6f4f5c
eddiechapman <eddie.chapman4@gmail.com>
2018-10-08 12:45:24 -0500

    variable and method headers

--- hw5/hw5/Pokemon.java
+++ hw5/hw5/Pokemon.java
@@ -1,7 +1,74 @@
+/********************************************************************************
+ * Pokemon.java
+ * Eddie Chapman
+ * 
+ * TODO: class description
+ *******************************************************************************/
+
 package hw5;
 
 public abstract class Pokemon {
 	
-	//TODO
+	protected String name;
+	protected int health;
+	protected int power;
+	protected int level;
 	
-}
+	//***************************************************************************
+	
+	public Pokemon(String name, int health, int power, int level)
+	{
+		//TODO
+	} // end Pokemon
+	
+	//***************************************************************************
+	
+	public String getName()
+	{
+		//TODO
+	}
+	
+	public int getHealth()
+	{
+		//TODO
+	}
+	
+	public void setHealth(int health)
+	{
+		//TODO
+	}
+	
+	public int getPower()
+	{
+		//TODO
+	}
+	
+	public void setPower(int power)
+	{
+		//TODO
+	}
+	
+	public int getLevel()
+	{
+		//TODO
+	}
+	
+	public void setLevel(int level)
+	{
+		//TODO
+	}
+	
+	//***************************************************************************
+	
+	public boolean isDefeated()
+	{
+		//TODO
+	} // end isDefeated
+	
+	//***************************************************************************
+	
+	protected void hurt(int damage)
+	{
+		//TODO
+	} // end hurt
+} // end class Pokemon
