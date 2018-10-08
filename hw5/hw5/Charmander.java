e5c7889a6de2721647cf791b465bb94567e062d7
eddiechapman <eddie.chapman4@gmail.com>
2018-10-08 12:53:09 -0500

    Variables and method headings

--- hw5/hw5/Charmander.java
+++ hw5/hw5/Charmander.java
@@ -1,7 +1,51 @@
+/********************************************************************************
+ * Charmander.java
+ * Eddie Chapman
+ * 
+ * TODO: class description
+ *******************************************************************************/
+
 package hw5;
 
 public class Charmander extends Pokemon {
 
-	//TODO
+	private final int fireBall = 5;
+	private final int bite = 4;
 	
-}
+	//***************************************************************************
+	
+	public Charmander(int health, int power, int level)
+	{
+		//TODO
+	} // end constructor
+	
+	//***************************************************************************
+	
+	@Override
+	public String toString()
+	{
+		//TODO
+	} // end toString
+	
+	//***************************************************************************
+	
+	@Override
+	public boolean equals(Object other)
+	{
+		//TODO
+	} // end equals
+	
+	//***************************************************************************
+	
+	public void specialAttack(Pokemon target)
+	{
+		//TODO
+	} // end specialAttack
+	
+	//***************************************************************************
+	
+	public void physicalAttack(Pokemon target)
+	{
+		//TODO
+	} // end physicalAttack
+} // end class Pikachu
