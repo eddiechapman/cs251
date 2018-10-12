0b157ffd3f60c472dedcb19e0da854cbfa9065ef
eddiechapman <eddie.chapman4@gmail.com>
2018-10-12 17:17:28 -0500

    style formatting

--- hw5/hw5/Charmander.java
+++ hw5/hw5/Charmander.java
@@ -29,7 +29,7 @@ public class Charmander extends Pokemon {
 				"\nHealth: " + health +
 				"\nPower: " + power;
 		
-	} // end toString
+	} // end toString 
 	
 	//***************************************************************************
 	
