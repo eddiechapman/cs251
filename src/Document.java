bb4ecaeae06446c6685fcbeb3eafb5f3ccc26464
eddiechapman <eddie.chapman4@gmail.com>
2018-11-12 15:02:13 -0600

    method headings

--- src/Document.java
+++ src/Document.java
@@ -1,7 +1,46 @@
 
 public class Document 
 {
-
-	//TODO - everything
+	private int docID;
+	private String docName;
+	
+	//***************************************************************************
+	
+	public Document(int docID, String docName) 
+	{
+//		this.docID = docID;
+//		this.docName = docName;
+	}
+	
+	//***************************************************************************
+	
+	public String getName()
+	{
+		
+	}
+	
+	//***************************************************************************
+	
+	public int getID()
+	{
+		
+	}
+	
+	//***************************************************************************
+	
+	@Override
+	public String toString()
+	{
+		
+	}
+	
+	//***************************************************************************
+	
+	@Override
+	public boolean equals(Object o)
+	{
+		
+	}
+	
 	
 } // end class Document
